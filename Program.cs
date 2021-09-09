c3fa834fbc801e5cacd95e414c4789060413f7cd
pknowledge <abhishekrawane1195@gmail.com>
2021-09-09 21:51:27 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace day4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Class1.GetAttendence();
+            //UC2_EmpWage.EmpWage();
+            //UC3_PartTimeEmpWage.PartTimeEmpWage();
+            //UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
+            //UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
+            //UC6_CalculateWageTill100HrsOr20IsReached.CalculateWagesFor20DaysInMonth();
+            //UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
+            UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("Hero", 100 , 22, 160);
+            Console.ReadKey();
+        }
+       
+        
+    }
+}
+3
